d53c9bfe7a480746ba6126b01ad16bb3957a69fa
Hitesh-Sehgal <48756482+Hitesh-Sehgal@users.noreply.github.com>
2019-09-01 19:01:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports= {
     'secret': 'secretkey',
     'database': 'mongodb://localhost/quiz',
-    "mongo_url": "mongodb+srv://hitesh:hitesh@cluster0-9tt3q.mongodb.net/test?retryWrites=true&w=majority"
+    "mongo_url": "mongodb+srv://<username>:<password>@cluster0-9tt3q.mongodb.net/test?retryWrites=true&w=majority"
 };
